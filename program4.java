25feaf992a5673ea6553642c55af74d46e3f5757
HunterPalli <158235663+HunterPalli@users.noreply.github.com>
2024-02-20 10:59:12 -0500

    Create program4.java
    program4 java file to add code to

---
+++ program4.java
@@ -0,0 +1,16 @@
+package accidentpack;
+
+import java.util.*;
+
+/**
+ * Program 4 file where the simulation will take place
+ *
+ */
+public class program4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
